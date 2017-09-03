1268efde81a594bc67c0250942c5629fcc38b0b9
Francisco Azevedo Alves <aaalves.francisco@gmail.com>
2017-09-02 22:09:31 -0300

    Adiciona aula 3

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,26 +1,35 @@
 var titulo = document.querySelector(".titulo-tabela");
 titulo.textContent = "Meus queridos e amados pacientes";
 
-var paciente = document.querySelector("#primeiro_paciente");
-var peso = paciente.querySelector(".info-peso").textContent;
-var altura = paciente.querySelector(".info-altura").textContent;
+var pacientes = document.querySelector("#tabela-pacientes").querySelectorAll(".paciente");
+console.log(pacientes);
 
-var pesoValido = true;
-var alturaValida = true;
-
-if (peso < 0 || peso > 1000) {
-    paciente.querySelector(".info-peso").textContent = "Peso inválido!";
-    pesoValido = false;    
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    
+    var pesoValido = true;
+    var alturaValida = true;
+    
+    if (peso < 0 || peso > 1000) {
+        paciente.querySelector(".info-peso").textContent = "Peso inválido!";
+        pesoValido = false;    
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if (altura < 0 || altura > 3.0) { 
+        paciente.querySelector(".info-altura").textContent = "Altura inválida!";
+        alturaValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if (pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        paciente.querySelector(".info-imc").textContent = imc.toFixed(2);
+    } else {
+        paciente.querySelector(".info-imc").textContent = "Paciente inválido!";
+    }
+     
 }
 
-if (altura < 0 || altura > 3.0) { 
-    paciente.querySelector(".info-altura").textContent = "Altura inválida!";
-    alturaValida = false;    
-}
-
-if (pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-} else {
-    var imc = "Paciente inválido!";
-}
-paciente.querySelector(".info-imc").textContent = imc;
